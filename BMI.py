9ec4ac7ef7caa68dce1449c4226fc6e741751eb9
srujan.g <63249339+srujan215@users.noreply.github.com>
2021-06-19 00:08:46 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,12 +1,18 @@
-wt = float(input("Enter your weight(in Kg): "))
-ht = float(input("Enter your hieght(in  m): "))
-ht2 = ht*ht
-bmi = wt/ht2
-print("Your BMI is: ",bmi)
-if(18.5 < bmi and bmi >= 24.9 ):
-   print("You are in a healthy range")
-if(bmi > 24.9):
-   print("You are overweighed")
-if(bmi < 18.5):
-   print("You are below a healthy range")
+def bmi(ht,wt):
+    bmi = wt/pow(ht,2)
+    return bmi
+
+def main():
+    a=float(input("Enter H: "))
+    b=float(input("Enter W: "))
+    n = bmi(a,b)
+    print(n)
+    if(18.5 < n and n >= 24.9 ):
+       print("You are in a healthy range")
+    if(n > 24.9):
+        print("You are overweighed")
+    if(n < 18.5):
+        print("You are underweighted")
+        
+main()
 
